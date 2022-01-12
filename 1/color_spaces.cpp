a9265b27bab01321a0c97f93e44b5b34e0fa00b4
Ana Cardenas <acardenasgasca@ucsb.edu>
2022-01-11 20:39:04 -0800

    some kind of transition working

--- 1/color_spaces.cpp
+++ 1/color_spaces.cpp
@@ -13,6 +13,7 @@ smooth.  This is because interpolation is done on the GPU.
 #include <vector>
 
 #include "al/app/al_App.hpp"
+#include "al/math/al_Functions.hpp"
 #include "al/graphics/al_Image.hpp"
 
 using namespace al;
@@ -20,14 +21,14 @@ using namespace std;
 
 class MyApp : public App {
 public:
-  Texture texture;
   Mesh mesh;
-
+  // Mesh wire;
+  int keyMode;
 
   void onCreate() {
+    keyMode = 1;
     // Load a .jpg file
-    //
-    const char *filename = "./data/hubble.jpg";
+    const char *filename = "./data/colors.jpg";
 
     auto imageData = Image(filename);
 
@@ -37,37 +38,62 @@ public:
     cout << "loaded image size: " << imageData.width() << ", "
          << imageData.height() << endl;
 
-    texture.create2D(imageData.width(), imageData.height());
-    texture.submit(imageData.array().data(), GL_RGBA, GL_UNSIGNED_BYTE);
-
-    texture.filter(Texture::LINEAR);
-
+    for (int j = 0; j < imageData.height(); ++j) {
+      for (int i = 0; i < imageData.width(); ++i) {
+        auto pixel = imageData.at(i, j);
+        // this does not seem to show anything
+        // float rNorm = ((int)pixel.r/255 * 2) -1;
+        // float gNorm = ((int)pixel.r/255 * 2) -1;
+        // float bNorm = ((int)pixel.r/255 * 2) -1;
+        // float rNorm = ((int)pixel.r)/10 - 25/2;
+        // float gNorm = ((int)pixel.g)/10 - 25/2;
+        // float bNorm = ((int)pixel.b)/10 - 25/2;
+        mesh.vertex(i/10,j/10,0);
+        mesh.color((int)pixel.r,(int)pixel.g,(int)pixel.b);
+      }
+    }
     // Generate the geometry onto which to display the texture
-    mesh.primitive(Mesh::TRIANGLE_STRIP);
-    mesh.vertex(-1, 1);
-    mesh.vertex(-1, -1);
-    mesh.vertex(1, 1);
-    mesh.vertex(1, -1);
+    mesh.primitive(Mesh::POINTS);
+    nav().pullBack(100);
 
-    // Add texture coordinates
-    mesh.texCoord(0, 1);
-    mesh.texCoord(0, 0);
-    mesh.texCoord(1, 1);
-    mesh.texCoord(1, 0);
-
-    nav().pullBack(4);
   }
 
   void onDraw(Graphics &g) {
+    //color of bakcgrund
     g.clear(0.2f);
-
-     // We must tell the GPU to use the texture when rendering primitives
-    texture.bind();
-    g.texture(); // Use texture for mesh coloring
+    // size of point primitive
+    g.pointSize(8);
+    // need to call this to show colors
+    g.meshColor();
+     // draw the mesh
     g.draw(mesh);
-    texture.unbind();
+    // g.draw(wire);
+  }
+
+  bool onKeyDown(const Keyboard &k) {
+    if (k.key() == ' ') {
+      // Press space bar to change mode
+    } else if (k.key() == 's') {
+      // Press s to change mode
+    }
+    return true;
+  }
+
+  void onAnimate(double dt_ms) {
+    if (keyMode == 1) {
+      auto& vertex = mesh.vertices(); // 'vertex' becomes an alias for 'mesh.vertices()'
+      auto& colors = mesh.colors();
+      for (int i = 1; i < vertex.size(); i++) {
+        float rNorm = ((int)colors[i].r)/10 - 25/2;
+        float gNorm = ((int)colors[i].g)/10 - 25/2;
+        float bNorm = ((int)colors[i].b)/10 - 25/2;
+        Vec3f newPos = Vec3f(rNorm,gNorm,bNorm);
+        vertex[i].lerp(newPos, 0.01);
+      }
+    }
   }
 };
+
 
 int main() {
   MyApp app;
