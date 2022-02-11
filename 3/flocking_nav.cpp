59bdcf49af5646afe5985db9573f2f1e705c467c
Ana Cardenas <acardenasgasca@ucsb.edu>
2022-02-10 18:30:46 -0800

    Add separation/cohesion/alignment but not working

--- 3/flocking_nav.cpp
+++ 3/flocking_nav.cpp
@@ -23,7 +23,7 @@ Vec3f wrapVec3f(Vec3f val, float hi, float lo) {
   return Vec3f(wrap(val.x, hi, lo), wrap(val.y, hi, lo), wrap(val.z, hi, lo));
 }
 
-static const int Nb = 500;  // Number of boids
+static const int Nb = 1000;  // Number of boids
 static const int cubeSize = 3;  // Number of boids
 
 // A "boid" (play on bird) is one member of a flock.
@@ -34,7 +34,7 @@ class Boid {
   float maxspeed;
 
   void init() {
-    maxspeed = rnd::uniform(0.01,0.02);
+    maxspeed = rnd::uniform(0.01,0.2);
     pose.pos(r(), r(), r());
     pose.quat().set(r(), r(), r(), r()).normalize();
   }
@@ -48,8 +48,10 @@ class Boid {
 
 struct MyApp : App {
   Parameter timeScale{"Time Sacale", 1, 0.01, 10};
-  Parameter nearRedius{"Near Radius", 1, 0.5, 2};
-  Parameter nearStrenght{"Near Steer Strenght", 0.5, 0.0, 1};
+  Parameter cohesionStrenght{"Cohesion Strenght", 0.5, 0.0, 1};
+  Parameter separationRadius{"Separation Radius", 1, 0.5, 2};
+  Parameter separationStrenght{"Separation Strenght", 0.3, 0.0, 1};
+  Parameter alignmentStrenght{"Alignment Strenght", 0.5, 0.0, 1};
 
   Boid boids[Nb];
   Mesh mesh;
@@ -59,8 +61,9 @@ struct MyApp : App {
     auto GUIdomain = GUIDomain::enableGUI(defaultWindowDomain());
     auto& gui = GUIdomain->newGUI();
     gui.add(timeScale);  // add parameter to GUI
-    gui.add(nearRedius);  // add parameter to GUI
-    gui.add(nearStrenght);  // add parameter to GUI
+    gui.add(cohesionStrenght);  // add parameter to GUI
+    gui.add(separationRadius);  // add parameter to GUI
+    gui.add(separationStrenght);  // add parameter to GUI
   }
 
   void resetBoids() {
@@ -98,22 +101,36 @@ struct MyApp : App {
     //
     for (int i = 0; i < Nb; i++)
     {
-        Vec3f nearBoidsCenter(0, 0, 0);
-        int count = 0;
+        Vec3f cohesionCenter(0, 0, 0);
+        Vec3f alignmentCenter(0, 0, 0);
+        int cohesionCount = 0;
+        Vec3f separationCenter(0, 0, 0);
+        int separationCount = 0;
         Boid main = boids[i];
         for (int j = 0; j < Nb; j++)
         {
-            if ( i!= j && ( main.pose.pos() - boids[j].pose.pos()).mag() < nearRedius) {
-                nearBoidsCenter += boids[j].pose.pos();
-                count++;
+            float distance = (main.pose.pos() - boids[j].pose.pos()).mag();
+            if ( i!= j && distance > separationRadius + 0.2 && distance < separationRadius + 1.2) {
+                cohesionCenter += boids[j].pose.pos();
+                cohesionCount++;
+                // but ! this is in the agent's frame of reference
+                alignmentCenter += boids[j].pose.pos() + boids[j].pose.quat().rotate(Vec3d(0, 0, -1));
+
+            }
+            if (i!= j && distance < separationRadius) {
+                separationCenter += boids[j].pose.pos();
+                separationCount ++;
             }
         }
-        if (count > 0) {
-            Vec3f centeringPos = nearBoidsCenter/count;
-            if (i ==0) {
-                cout <<centeringPos << endl;
-            } 
-            main.pose.faceToward(centeringPos,Vec3d(0, 1, 0), nearStrenght);
+        if (cohesionCount > 0) {
+            Vec3f centeringPos = cohesionCenter/cohesionCount;
+            main.pose.nudgeToward(centeringPos, cohesionStrenght);
+            Vec3f alignmentPos = alignmentCenter/cohesionCount;
+            main.pose.faceToward(alignmentPos, Vec3d(0, 1, 0), alignmentStrenght);
+        }
+        if (separationCount > 0) {
+            Vec3f centeringPos = -separationCenter/separationCount;
+            main.pose.nudgeToward(centeringPos, separationStrenght);
         }
     }
   }
