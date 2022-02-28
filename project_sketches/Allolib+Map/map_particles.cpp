39315b60812b840c2786fdff120fb13d105f259b
Ana Cardenas <acardenasgasca@ucsb.edu>
2022-02-28 14:14:50 -0800

    Slow animation

--- project_sketches/Allolib+Map/map_particles.cpp
+++ project_sketches/Allolib+Map/map_particles.cpp
@@ -15,36 +15,56 @@ smooth.  This is because interpolation is done on the GPU.
 #include "al/app/al_App.hpp"
 #include "al/math/al_Functions.hpp"
 #include "al/graphics/al_Image.hpp"
+#include "al/math/al_Random.hpp"
 
 using namespace al;
 using namespace std;
 
 
+// struct FlowField {
+//     vector<Vec2f> vectors; // meters/second
+//     int resX;
+//     int resY;
+// };
+
 class MyApp : public App {
 public:
+  // FlowField field;
   Mesh mesh;
-  // Mesh wire;
+  vector<Vec3f> velocity;
+  vector<Vec3f> acceleration;
+
+  vector<float> victimValues;
+
   int imgWidth, imgHeight;
+
+  Vec3f randomVec3f(float scale) {
+    return Vec3f(rnd::uniformS(), rnd::uniformS(), rnd::uniformS()) * scale;
+  }
 
   void onCreate() {
     // Load a .jpg file
-    const char *filename = "./data/displacement.png";
+    const char *mapImage = "./data/displacement.png";
+    const char *victimsImage = "./data/dots_topography.png";
 
-    auto imageData = Image(filename);
+    auto mapData = Image(mapImage);
+    auto victimsData = Image(victimsImage);
 
-    if (imageData.array().size() == 0) {
+    if (mapData.array().size() == 0) {
       cout << "failed to load image" << endl;
       exit(1);
     }
-    cout << "loaded image size: " << imageData.width() << ", "
-         << imageData.height() << endl;
+    cout << "loaded image size: " << mapData.width() << ", "
+         << mapData.height() << endl;
 
-    imgWidth = imageData.width();
-    imgHeight = imageData.height();
+    // Asumes both images have the same size
+    imgWidth = mapData.width();
+    imgHeight = mapData.height();
 
     for (int j = 0; j < imgHeight; ++j) {
       for (int i = 0; i < imgWidth; ++i) {
-        auto pixel = imageData.at(i, j);
+        auto pixelV = victimsData.at(i, j);
+        auto pixel = mapData.at(i, j);
          // remap from 0, width to -1,1
         float x = map(-1,1,0,imgWidth,(float)i);
         float y = map(1,-1,0,imgHeight,(float)j);
@@ -56,15 +76,39 @@ public:
         HSV hsvColor = HSV(color);
         float z = map(0,0.2,0,1,hsvColor.v);
 
+        float rV = map(0,1,0,255,(float)pixelV.r);
+        float gV = map(0,1,0,255,(float)pixelV.g);
+        float bV = map(0,1,0,255,(float)pixelV.b);
+        Color colorV = Color(rV,gV,bV);
+        HSV hsvColorv = HSV(colorV);
+        float valueV = map(0,0.2,0,1,hsvColorv.v);
+        victimValues.push_back(valueV);
+
         mesh.vertex(x,y,z);
         mesh.color(color);
+
+        velocity.push_back(0);
+        acceleration.push_back(0);
       }
+
+      // generateFieldVector();
     }
     // Generate the geometry onto which to display the texture
     mesh.primitive(Mesh::POINTS);
     nav().pullBack(4);
 
   }
+
+  // Takes too long with the size of the image
+  // void generateFieldVector() {
+  //   field = {vector<Vec2f>(), imgWidth , imgHeight};
+  //   for (int j = 0; j < field.resY; ++j) {
+  //     for (int i = 0; i < field.resX; ++i) {
+  //       Vec2f vect = Vec2f(rnd::uniform(), rnd::uniform());
+  //       // field.vectors.push_back(vect);
+  //     }
+  //   }
+  // }
 
   void onDraw(Graphics &g) {
     //color of bakcgrund
@@ -76,6 +120,28 @@ public:
      // draw the mesh
     g.draw(mesh);
     // g.draw(wire);
+
+    g.meshColor();
+    // Mesh fieldMesh(Mesh::LINES);
+    // for (int j = 0; j < field.resY; ++j) {
+    //   for (int i = 0; i < field.resX; ++i) {
+    //     Vec2f orientation = field.vectors[i*field.resX + j];
+    //     float originX = map(-1.f,1.f,0,field.resX,i);
+    //     float originY = map(-1.f,1.f,0,field.resY,j);
+    //     Vec3f originPoint = Vec3f(originX, originY, 0.f);
+    //     Vec3f endPoint = Vec3f(originX + orientation.x,
+    //                             originY + orientation.y,  0.f);
+
+    //     Color color = Color(1,0,0);
+
+    //     // here we're rendering a point based on the vector field
+    //     fieldMesh.vertex(originPoint);
+    //     fieldMesh.color(color);
+    //     fieldMesh.vertex(endPoint);
+    //     fieldMesh.color(color);
+    //   }
+    // }
+    // g.draw(fieldMesh);
   }
 
   bool onKeyDown(const Keyboard &k) {
@@ -83,6 +149,10 @@ public:
   }
 
   void onAnimate(double dt_ms) {
+    auto& vertex = mesh.vertices();
+    for (int i = 1; i < vertex.size(); i++) {
+      vertex[i].lerp(randomVec3f(1), 0.1);
+    }
   }
 
   float map (float min_d, float max_d, float min_o, float max_o, float x) {
